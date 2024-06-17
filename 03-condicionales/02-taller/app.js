69b41075f5982362ee8b2e158ad1284afb41cedb
halcos16 <alexcardona.majo@gmail.com>
2024-06-17 12:05:46 -0500

    actualizar

--- 03-condicionales/02-taller/app.js
+++ 03-condicionales/02-taller/app.js
@@ -67,16 +67,20 @@ if (cantidad >=1 && cantidad <=4){
 
 //Punto 5 taller
 
-/* alert("Lista de frutas:\n- Mango\n- Manzana\n- Pera\n- Melon\n- Piña");
-let frutaelegida = prompt("Ingresar la fruta que quieres").toLowerCase();
-let numero = prompt("Ingresa la cantidad");
-let precios = {
-    "Mango": 1000,
-    "Manzana": 1100,
-    "Pera": 1200,
-    "Melon": 1300,
-    "Piña": 1500,
-};
-let valorporunidad = precios[frutaelegida];
-let total = valorporunidad * numero;
-alert("Frutas: " + frutaelegida + "\nValor por unidad: $" + parseFloat(valorporunidad) + "\nValor total: $" + parseFloat(total)); */
+listado = alert ('Este es el listado de frutas disponibles Manzana 1000$, Pera 700$, Papaya 1500$, Banano 500$, Sandia 2000$, Melon 1900$');
+fruta1 = prompt ('Ingrese la cantidad de Manzanas que desea llevar');
+fruta2 = prompt ('Ingrese la cantidad de Peras que desea llevar');
+fruta3 = prompt ('Ingrese la cantidad de Papayas que desea llevar');
+fruta4 = prompt ('Ingrese la cantidad de Bananos que desea llevar');
+fruta5 = prompt ('Ingrese la cantidad de Sandias que desea llevar');
+fruta6 = prompt ('Ingrese la cantidad de Melones que desea llevar');
+let manzana, pera, papaya, banano, sandia, melon;
+manzana = 1000
+pera = 700
+papaya = 1500
+banano = 500
+sandia = 2000
+melon = 1900
+valorTotal = parseFloat(fruta1*manzana+fruta2*pera+fruta3*papaya+fruta4*banano+fruta5*sandia+fruta6*melon)
+    console.log ('Manzana '+manzana*fruta1, 'Pera '+pera*fruta2, 'Papaya '+papaya*fruta3, 'Banano '+banano*fruta4, 'Sandia '+sandia*fruta5, 'Melon '+melon*fruta6)
+    console.log ('El valor total de los productos elegidos es de: ' +valorTotal)
